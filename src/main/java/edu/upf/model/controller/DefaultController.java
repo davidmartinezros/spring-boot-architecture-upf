ae029cf9ea6226a08ecbdfca6feb83da601bc82e
David Martinez Ros <programador2.informatica@upf.edu>
2018-06-04 12:15:18 +0200

    configurem logback al spring pels 3 entorns i pintem la primera traça d'exemple

--- src/main/java/edu/upf/model/controller/DefaultController.java
+++ src/main/java/edu/upf/model/controller/DefaultController.java
@@ -3,13 +3,18 @@ package edu.upf.model.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
- 
+
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping(value = {"/"})
+@Slf4j
 public class DefaultController {
      
     @RequestMapping()
     public ModelAndView inici() {
+    	
+    	log.debug("Provant els logs");
          
         return new ModelAndView("inici");
  
